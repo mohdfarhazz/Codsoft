dee61fe39c3f6add0899d1dce09a2d5e5b7e0cca
mohdfarhazz <142298570+mohdfarhazz@users.noreply.github.com>
2024-01-08 23:04:02 +0530

    User Defined Calculator

---
+++ cal.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    char operation;
+
+    cout << "Welcome to the Basic Calculator!\n";
+    
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error\n";
+            }
+            break;
+        default:
+            cout << "Error: Invalid operation.\n";
+            break;
+    }
+
+    return 0;
+}
